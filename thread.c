a8826da6ae4d905ba40de7e6bd0b4faab8492e04
Pavel-Kr <pkrivosheev@bk.ru>
2022-08-18 15:46:47 +0700

    Threads

---
+++ thread.c
@@ -0,0 +1,22 @@
+#include <pthread.h>
+#include <stdio.h>
+int a;
+void* func(void* arg){
+    for(int i=0;i<1000000;i++){
+        int tmp=a;
+        tmp++;
+        a=tmp;
+    }
+    return 0;
+}
+int main(){
+    pthread_t tid[5];
+    void** status;
+    for(int i=0;i<5;i++){
+        pthread_create(&tid[i],NULL,func,NULL);
+    }
+    for(int i=0;i<5;i++){
+        pthread_join(tid[i],NULL);
+    }
+    printf("%d\n",a);
+}
