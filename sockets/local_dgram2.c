36b1715f2afd7b7e50b50a5d3d3b8fade5d8a034
Pavel-Kr <pkrivosheev@bk.ru>
2022-09-02 13:00:27 +0700

    part 4

--- sockets/local_dgram2.c
+++ sockets/local_dgram2.c
@@ -7,9 +7,17 @@
 #include <sys/un.h>
 #include <unistd.h>
 int main(){
+    unlink("sock_cl");
     int sockfd=socket(AF_LOCAL,SOCK_DGRAM,0);
     if(sockfd==-1){
         perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    struct sockaddr_un client;
+    client.sun_family=AF_LOCAL;
+    strcpy(client.sun_path,"sock_cl");
+    if(bind(sockfd,(struct sockaddr*)&client,sizeof(client))==-1){
+        perror("bind");
         exit(EXIT_FAILURE);
     }
     struct sockaddr_un serv;
@@ -27,5 +35,6 @@ int main(){
     }
     printf("%s\n",str);
     close(sockfd);
+    unlink("sock_cl");
     exit(EXIT_SUCCESS);
 }
